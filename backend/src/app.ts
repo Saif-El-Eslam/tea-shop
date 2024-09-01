fd098d7ec5627df689fd2f70043a938457212096
saifeleslam <saif82820@gmail.com>
2024-09-01 22:00:56 +0300

    added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authRoutes";
+import cors from "cors";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+
+app.use(cors());
 
 // Add routes with /api prefix
 app.use("/api/auth", authRoutes);
